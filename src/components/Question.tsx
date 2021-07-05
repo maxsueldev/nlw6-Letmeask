d529329f01a5795a265e0b4aef9d7baff14d7b17
Maxsuel Fernando <maxsuel.dev@gmail.com>
2021-07-05 20:23:57 -0300

    aula04

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 import '../styles/question.scss';
 
 type QuestionProps = {
@@ -5,13 +7,14 @@ type QuestionProps = {
   author: {
     name: string;
     avatar: string;
-  }
-
+  };
+  children?: ReactNode;
 }
 
 export function Question({
   content,
-  author
+  author,
+  children
 }: QuestionProps) {
   return (
     <div className="question">
@@ -20,6 +23,9 @@ export function Question({
         <div className="user-info">
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
+        </div>
+        <div>
+          {children}
         </div>
       </footer>
     </div>
